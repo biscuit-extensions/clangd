bd1164408add1f5ac2802b315ca1472bbc004877
billy <billydevbusiness@gmail.com>
2024-12-03 01:45:53 +0530

    Initial commit

---
+++ hello/hello.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import typing
+
+from biscuit.extensions import Extension
+
+if typing.TYPE_CHECKING:
+    from biscuit.api import ExtensionsAPI
+
+
+class Hello(Extension):
+    def __init__(self, api: ExtensionsAPI) -> None:
+        super().__init__(api)
+
+        self.api.logger.info(f"This is a sample log!")
+
+    def install(self) -> None:
+        self.api.notifications.info(f"Hello, Biscuit!")
---
+++ tests/test_hello.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+import typing
+import sys
+
+sys.path.append("..")
+
+from hello import setup
+
+if typing.TYPE_CHECKING:
+    from biscuit import App
+
+
+class TestExtension:
+    def test_init(self, app_instance: App):
+        assert app_instance is not None
+
+    def test_extension_setup(self, app_instance: App):
+        setup(app_instance.api)
+        assert True
